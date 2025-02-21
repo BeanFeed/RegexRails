02c60044dcaaba90d428064468baf621840556d3
BeanFeed <two.austin.two@gmail.com>
2025-02-20 21:24:00 -0500

    register listener

--- src/main/java/com/beanfeed/regexRails/RegexRails.java
+++ src/main/java/com/beanfeed/regexRails/RegexRails.java
@@ -1,19 +1,23 @@
 package com.beanfeed.regexRails;
 
 import com.beanfeed.regexRails.events.DetectorRailListener;
+import com.beanfeed.regexRails.events.PrepareAnvilListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RegexRails extends JavaPlugin {
     private DetectorRailListener minecartDetector;
+    private PrepareAnvilListener anvilListener;
 
     @Override
     public void onEnable() {
         minecartDetector = new DetectorRailListener(this);
+        anvilListener = new PrepareAnvilListener(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         minecartDetector = null;
+        anvilListener = null;
     }
 }
