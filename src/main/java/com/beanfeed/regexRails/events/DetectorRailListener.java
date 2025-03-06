87a1e26dec036ae747163f36c28b340dbd76d268
BeanFeed <two.austin.two@gmail.com>
2025-03-06 14:54:04 -0500

    Add lecturn feature

--- src/main/java/com/beanfeed/regexRails/events/DetectorRailListener.java
+++ src/main/java/com/beanfeed/regexRails/events/DetectorRailListener.java
@@ -6,6 +6,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.Lectern;
 import org.bukkit.block.Sign;
 import org.bukkit.block.data.AnaloguePowerable;
 import org.bukkit.block.data.type.RedstoneWire;
@@ -14,6 +15,8 @@ import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.Plugin;
 
 import java.util.logging.Logger;
@@ -56,23 +59,51 @@ public class DetectorRailListener implements Listener {
 
     private void handleDetection(Minecart minecart, Block rail, BlockRedstoneEvent event) {
         Sign sign = null;
+        Lectern lecturn = null;
         for (Faces face : Faces.values()) {
             Block adjacent = rail.getRelative(face.modX, face.modY, face.modZ);
             //logger.info("Adjacent block: " + adjacent.getType());
             if (adjacent.getState() instanceof Sign s) {
                 sign = s;
                 break;
+            } else if (adjacent.getState() instanceof Lectern l) {
+                lecturn = l;
+                break;
             }
         }
 
-        if(sign == null) return;
+        if(sign == null && lecturn == null) return;
 
-        String[] lines = sign.getSide(Side.FRONT).getLines();
+        String[] lines;
 
         //Modes: "compare", "append", "set"
-        String mode = lines[0].toLowerCase().trim();
+        String mode;
 
         //logger.info("Mode: -" + mode + "-");
+
+        if(sign != null) {
+            lines = sign.getSide(Side.FRONT).getLines();
+        } else {
+            ItemStack book = lecturn.getInventory().getItem(0);
+            if (book == null || !book.hasItemMeta() || !(book.getItemMeta() instanceof BookMeta bookMeta)) {
+                return;
+            }
+
+            int currentPage = lecturn.getPage();
+
+            if (bookMeta.getPageCount() < currentPage) {
+                return;
+            }
+
+            String pageContent = bookMeta.getPage(currentPage + 1);
+            lines = pageContent.split("\n", 3); // Split by newlines, max 3 lines
+
+            if (lines.length == 0) {
+                return;
+            }
+
+        }
+        mode = lines[0].toLowerCase().trim();
 
         if (mode.equals("compare")) {
             String compareType = lines[1];
@@ -99,7 +130,9 @@ public class DetectorRailListener implements Listener {
         } else {
             boolean activated = false;
             for (Faces face : Faces.values()) {
-                Block adjacent = sign.getBlock().getRelative(face.modX, face.modY, face.modZ);
+                Block adjacent;
+                if(sign != null) adjacent = sign.getBlock().getRelative(face.modX, face.modY, face.modZ);
+                else adjacent = lecturn.getBlock().getRelative(face.modX, face.modY, face.modZ);
                 if(adjacent.getType() == Material.DETECTOR_RAIL) continue;
                 if (adjacent.isBlockPowered() || adjacent.isBlockIndirectlyPowered()) {
                     activated = true;
